9c0ed077c56c2a2b2be175489cf1fd7c7ded63c2
Caua-Souza-Silva <caua568@outlook.com>
2023-08-19 05:37:11 -0300

    Creating entity Users

---
+++ src/main/java/com/example/ecommerce/model/entities/User.java
@@ -0,0 +1,42 @@
+package com.example.ecommerce.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "user_name", unique = true, nullable = false)
+    private String userName;
+
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Boolean deleted;
+}
---
+++ src/main/resources/db/migration/V1__CREATE_TABLE_USERS.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+	id varchar(20) NOT NULL,
+	user_name varchar(255) NOT NULL,
+	cpf varchar(18) NOT NULL,
+	email varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
+	deleted bit(1) DEFAULT 0 NOT NULL,
+	CONSTRAINT users_PK PRIMARY KEY (id),
+	CONSTRAINT users_UN UNIQUE KEY (user_name,cpf,email)
+)
