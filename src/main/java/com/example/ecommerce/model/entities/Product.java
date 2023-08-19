137088f02b91c396b50287349639300611063f95
Caua-Souza-Silva <caua568@outlook.com>
2023-08-19 05:38:18 -0300

    Creating entity Product

---
+++ src/main/java/com/example/ecommerce/model/entities/Product.java
@@ -0,0 +1,46 @@
+package com.example.ecommerce.model.entities;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Boolean deleted;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "seller_id", referencedColumnName = "id", nullable = false)
+    private User seller;
+}
