abde02611a10ed15b077d0d6c1556ec23304d6ca
Caua-Souza-Silva <caua568@outlook.com>
2023-08-24 20:56:05 -0300

    Implementing Item Service

---
+++ src/main/java/com/example/ecommerce/model/services/ItemService.java
@@ -0,0 +1,51 @@
+package com.example.ecommerce.model.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ecommerce.model.DTOs.ItemDTO;
+import com.example.ecommerce.model.DTOs.ProductItemDTO;
+import com.example.ecommerce.model.entities.Item;
+import com.example.ecommerce.model.entities.Product;
+import com.example.ecommerce.model.repositories.ItemRepository;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemRepository repository;
+
+    @Autowired
+    private ProductService productService;
+
+    public List<ItemDTO> mapperListItemToListDTO(List<Item> items) {
+        List<ItemDTO> dtos = new ArrayList<>();
+        items.forEach(i -> dtos.add(mapperItemToDTO(i)));
+        return dtos;
+    }
+
+    public Item createItem(ProductItemDTO item) {
+        Product product = productService.findProductById(item.productId());
+        Item entity = repository.save(new Item(
+                null,
+                product,
+                item.amount(),
+                item.variation()));
+        return entity;
+    }
+
+    private ItemDTO mapperItemToDTO(Item item) {
+        return new ItemDTO(
+                item.getId(),
+                item.getProduct().getName(),
+                item.getProduct().getDescription(),
+                item.getProduct().getPrice(),
+                item.getProduct().getFreightPrice(),
+                item.getAmount(),
+                item.getVariation(),
+                item.getProduct().getSeller().getUserName());
+    }
+}
